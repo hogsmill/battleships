888c2cda80e14751bcd6428a31a3978bec85b672
hogsmill <info@hogsmill.com>
2021-06-11 08:33:43 +0100

    styling

--- src/components/OtherName.vue
+++ src/components/OtherName.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="other-name" v-if="!showAbout">
-    <span v-if="!theirName"><i>You need to get another player to join the game...</i></span>
+    <div v-if="myName && !theirName" class="need-other"><i>You now need to get another player to join the same game...</i></div>
     <span v-if="gameSet" class="rounded mr-2 mt-2 pointer p-2 bg-light">I am playing: {{ theirName.name }}</span>
   </div>
 </template>
@@ -20,6 +20,9 @@ export default {
     gameState() {
       return this.$store.getters.getGameState
     },
+    myName() {
+      return this.$store.getters.getMyName
+    },
     theirName() {
       return this.$store.getters.getTheirName
     }
@@ -30,6 +33,14 @@ export default {
 <style lang="scss">
   .other-name {
     width: 200px;
-    display: inline-block
+    display: inline-block;
+
+    .need-other {
+      position: relative;
+      top: 12px;
+      width: 245px;
+      color: #fff;
+      font-weight: bold;
+    }
   }
 </style>
