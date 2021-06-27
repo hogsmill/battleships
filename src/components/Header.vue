aab1d489e5408a44b4d7610b63195fd5a4e46d84
hogsmill <info@hogsmill.com>
2021-06-27 13:03:04 +0100

    login

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -112,13 +112,14 @@ export default {
     if (session) {
       session = JSON.parse(session)
       this.$store.dispatch('updateSession', session.session)
-      console.log('sendCheckLogin', {id: this.id, session: session})
-      bus.$emit('sendCheckLogin', {id: this.id, session: session})
+      console.log('sendCheckLogin', {session: session})
+      bus.$emit('sendCheckLogin', {session: session})
     } else {
       this.$store.dispatch('updateSession', '')
     }
 
     bus.$on('loginSuccess', (data) => {
+      console.log(data)
       this.$store.dispatch('updateSession', data.session)
       this.$store.dispatch('updateUserName', data.userName)
       this.$store.dispatch('updateAdmin', data.loggedInAsAdmin)
